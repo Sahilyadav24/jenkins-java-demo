96cf98bc0021ae8fc27ab6ce16efc46b2410db41
Sahilyadav24 <sahilrao2424@gmail.com>
2025-09-04 13:55:08 +0530

    Initial Java Maven webapp

---
+++ src/main/java/com/demo/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Hello from Jenkins CI/CD with Tomcat!</h2>");
+    }
+}
